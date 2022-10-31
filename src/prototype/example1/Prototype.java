d8850aff55dff9e8eb889ddd80ef35460b338b64
Tahseen <tahseen.mynt@perennialsys.com>
2022-10-31 16:11:43 +0530

    Prototype example

---
+++ src/prototype/example1/Prototype.java
@@ -0,0 +1,12 @@
+/**
+ * Globe FinTech Innovations, Inc.
+ * Copyright (c) 2004-2022 All Rights Reserved.
+ */
+package prototype.example1;
+
+
+interface Prototype {
+
+    public Prototype getClone();
+
+}
